17640896a23de764a4aa2afbc9dfbdf7eb375178
Akanksha Pandit <akankshapandit235@gmail.com>
2026-02-15 14:19:49 +0530

    Update API URL for production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://realtime-poll-app-qm5w.onrender.com/api",
 });
 
 export default api;
